04b1f573bb9a0c2f5c3fe0e811450a9e50d5a3e2
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-10-12 20:39:47 -0300

    orphanages map page

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import mapMarkerImg from '../assets/images/map-marker.svg';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import '../styles/pages/orphanages-map.css';
+import { Map, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export default function OrphanagesMap() {
   return (
-    <h1>
-      Hello World map
-    </h1>
-  )
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy" />
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>
+            Muitas crianças estão esperando a sua visita :)
+          </p>
+        </header>
+
+        <footer>
+          <strong>Florianópolis</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+
+      <Map center={[-27.5853379, -48.5862306]} zoom={12} style={{ width: '100%', height: '100%' }}>
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
 }
