7765e641613323cb5d714c4fd4f70804ffef2afa
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-10-13 20:47:31 -0300

    create orphanage route

--- backend/src/database/migrations/1602630707972-create_orphanage.ts
+++ backend/src/database/migrations/1602630707972-create_orphanage.ts
@@ -39,6 +39,10 @@ export class createOrphanage1602630707972 implements MigrationInterface {
 					type: 'text'
 				},
 				{
+					name: 'opening_hours',
+					type: 'varchar'
+				},
+				{
 					name: 'open_on_weekends',
 					type: 'boolean',
 					default: false
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
 import './database/connection';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get('/users/:id', (req, res) => {
-  return res.status(200).json({ message: 'hello world' });
-});
+app.use(routes);
 
 app.listen(3333);
