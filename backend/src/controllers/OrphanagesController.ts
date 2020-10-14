1e5735b54beca37db4fc80465b16dabe53c1a0bb
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-10-14 18:59:35 -0300

    Organizing endpoints responses (views)

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,22 +1,27 @@
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import { Request, Response } from 'express';
+import OrphanagesView from '../views/orphanage_views';
 
 export default {
   async index(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanages = await orphanagesRepository.find({});
+    const orphanages = await orphanagesRepository.find({
+      relations: ['images']
+    });
 
-    return res.status(200).json(orphanages);
+    return res.status(200).json(OrphanagesView.renderMany(orphanages));
   },
 
   async show(req: Request, res: Response) {
     const orphanagesRepository = getRepository(Orphanage);
 
-    const orphanage = await orphanagesRepository.findOneOrFail(req.params.id);
+    const orphanage = await orphanagesRepository.findOneOrFail(req.params.id, {
+      relations: ['images']
+    });
 
-    return res.status(200).json(orphanage); // TODO tratar
+    return res.status(200).json(OrphanagesView.render(orphanage)); // TODO tratar
   },
 
   async create(req: Request, res: Response) {
